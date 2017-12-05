7177fa0f56c34ae06ec87874ca79ac496737471d
harryyang1982 <harry.seunghoon.yang@gmail.com>
2017-12-05 15:55:50 +0900

    chapter 11 added

--- codes/chapter11.R
+++ codes/chapter11.R
@@ -266,3 +266,165 @@ identical(no_vowels_1, no_vowels_2)
 
 words[str_detect(words, "x$")]
 str_subset(words, "x$")
+
+df <- tibble(
+  word = words,
+  i = seq_along(word)
+)
+
+df %>% 
+  filter(str_detect(words, "x$"))
+
+x <- c("apple", "banana", "pear")
+str_count(x, "a")
+
+mean(str_count(words, "[aeiou]"))
+
+df %>% 
+  mutate(
+    vowels = str_count(word, "[aeiou]"),
+    consonants = str_count(word, "[^aeiou]")
+  )
+
+str_count("abababa", "aba")
+str_view_all("abababa", "aba")
+
+## Exercises
+#1a
+words[str_detect(words, "^x|x$")]
+#1b
+words[str_detect(words, "^[aeiou]") & str_detect(words, "[^aeiou]$")]
+#1c
+words[str_detect(words, "a") &
+        str_detect(words, "e") &
+        str_detect(words, "i") &
+        str_detect(words, "o") &
+        str_detect(words, "u")]
+#1d
+DFF <- tibble(word = words,
+  vowel_count = str_count(words, "[aeiou]"),
+  consonant_count = str_count(words, "[^aeiou]"))
+DFF %>% 
+  arrange(desc(vowel_count)) %>% 
+  head(10)
+
+DFF %>% 
+  arrange(desc(proportion)) %>% 
+  head(10)
+
+## Extract Matches
+
+length(sentences)
+head(sentences)
+
+colors <- c(
+  "red", "orange", "yellow", "green", "blue", "purple"
+)
+
+color_match <- str_c(colors, collapse = "|")
+color_match
+
+has_color <- str_subset(sentences, color_match)
+matches <- str_extract(has_color, color_match)
+head(matches)
+
+more <- sentences[str_count(sentences, color_match) > 1]
+str_view_all(more, color_match)
+str_extract(more, color_match)
+
+str_extract_all(more, color_match)
+str_extract_all(more, color_match, simplify = T)
+
+x <- c("a", "a b", "a b c")
+str_extract_all(x, "[a-z]", simplify = T)
+str_extract_all(x, "[a-z]")
+
+## Exercises
+
+#1
+color_match2 <- str_c("\\b(", str_c(colors, collapse = "|"), ")\\b")
+color_match2
+
+more2 <- sentences[str_count(sentences, color_match2) > 1]
+str_view_all(more2, color_match2)
+str_view_all(has_color, color_match2)
+
+#2a
+str_extract(sentences, "[a-z]")
+word(sentences, 1)
+#2b
+str_extract(sentences, ".+ing.$")
+#2c
+str_extract(sentences, ".+s.$")
+
+is_noun <- str_subset(sentences, "(a|the) [^ \\.]+")
+nouns <- str_extract(is_noun, "(a|the) [^ \\.]+")
+is_plural <- str_subset(nouns, "(a|the) [^ \\.\\']+(s|\\')$")
+str_extract(is_plural, "(a|the) [^ \\.\\']+(s|\\')$")
+
+## Grouped Matches
+
+noun <- "(a|the) ([^ ]+)"
+has_noun <- sentences %>% 
+  str_subset(noun) %>% 
+  head(10)
+has_noun %>% 
+  str_extract(noun)
+
+has_noun %>% 
+  str_match(noun)
+
+tibble(sentence = sentences) %>% 
+  extract(
+    sentence, c("article", "noun"), "(a|the) ([^ ]+)",
+    remove = F
+  )
+
+## Exercises
+
+#1
+tibble(sentence = sentences) %>% 
+  extract(
+    sentence, c("number", "word"), "(one|two|three|four|five|six|seven|eight|nine|ten) ([^ ]+)",
+    remove = F
+  ) %>% 
+  na.omit(number)
+
+#2
+contraction <- "([A-Za-z]+)'([A-Za-z]+)"
+sentences %>% 
+  str_subset(contraction)
+
+## Replacing Matches
+
+x <- c("apple", "pear", "banana")
+str_replace(x, "[aeiou]", "-")
+str_replace_all(x, "[aeiou]", "-")
+
+x <- c("1 house", "2 cars", "3 people")
+str_replace_all(x, c("1" = "one", "2" = "two", "3" = "three"))
+
+sentences %>% 
+  str_replace("([^ ]+) ([^ ]+) ([^ ]+)", "\\1 \\3 \\2") %>% 
+  head(5)
+
+## Exercises
+
+#1
+str = "heoagjt/jgoiwe/\\/geoa"
+writeLines(str_replace_all(str, "/", "\\\\"))
+
+str = "fuckyouall/babo///busain///killus/////kkkk"
+str
+
+#2
+str_replace_all(sentences[1:10], c("A" = "a", "B" = "b", "C" = "c", "D" = "d", "E" = "e", "F" = "f",
+                                   "G" = "g", "H" = "h", "I" = "i", "J" = "j", "K" = "k", "L" = "l",
+                                   "M" = "m", "N" = "n", "O" = "o", "P" = "p", "Q" = "q", "R" = "r",
+                                   "S" = "s", "T" = "t", "U" = "u", "V" = "v", "W" = "w", "X" = "x",
+                                   "Y" = "y", "Z" = "z"))
+
+#3
+words %>% 
+  str_replace("^(.)(.*)(.)$", "\\3 \\2 \\1") %>% 
+  head(5)
