1d5685fab18aa8af39c4ab54dfb37f14f5fdc999
Harry Yang <harry.seunghoon.yang@gmail.com>
2017-12-21 17:04:26 +0900

    chapter 17 cleared

--- codes/chapter17.R
+++ codes/chapter17.R
@@ -304,3 +304,297 @@ col_summary(df, median)
 col_summary(df, mean)
 
 # The Map Functions
+
+map_dbl(df, mean)
+map_dbl(df, median)
+map_dbl(df, sd)
+
+df %>% map_dbl(mean)
+df %>% map_dbl(median)
+df %>% map_dbl(sd)
+df %>% map_dbl(var)
+
+map_dbl
+
+map_dbl(df, mean, trim = 0.5)
+
+z <- list(x = 1:3, y = 4:5)
+map_int(z, length)
+
+## Shortcuts
+### Verbose way
+models <- mtcars %>% 
+  split(.$cyl) %>% 
+  map(function(df) lm(mpg ~ wt, data = df))
+### Concise way
+models <- mtcars %>% 
+  split(.$cyl) %>% 
+  map(~lm(mpg ~ wt, data = .))
+
+### Traditional way
+models %>% 
+  map(summary) %>% 
+  map_dbl(~.$r.squared)
+### String way
+models %>% 
+  map(summary) %>% 
+  map_dbl("r.squared")
+
+x <- list(list(1, 2, 3), list(4, 5, 6), list(7, 8, 9))
+x %>% map_dbl(2)
+
+## Base R
+
+x1 <- list(
+  c(0.27, 0.37, 0.57, 0.91, 0.20),
+  c(0.90, 0.94, 0.66, 0.63, 0.06),
+  c(0.21, 0.18, 0.69, 0.38, 0.77)
+)
+x2 <- list(
+  c(0.50, 0.72, 0.99, 0.38, 0.78),
+  c(0.93, 0.21, 0.65, 0.13, 0.27),
+  c(0.39, 0.01, 0.38, 0.87, 0.34)
+)
+threshold <- function(x, cutoff = 0.8) x[x > cutoff]
+x1 %>% sapply(threshold) %>% str()
+x2 %>% sapply(threshold) %>% str()
+
+## Exercises
+
+#1a
+mtcars %>% map_dbl(mean)
+#1b
+nycflights13::flights %>% map(class)
+#1c
+iris %>% map(unique)
+#1d
+rn_df <- c(-10, 0, 10, 100)
+rn_df %>% map(~rnorm(10, mean = ., sd = 1))
+
+map(c(-10, 0, 10, 100), rnorm, n = 10)
+
+#2
+mpg$cyl <- as.factor(mpg$cyl)
+mpg$trans <- as.factor(mpg$trans)
+
+fact_ver <- mpg %>% map_lgl(is.factor)
+str(fact_ver)
+
+#3
+map(1:5, runif)
+
+#4
+map(-2:2, rnorm, n =5)
+map_dbl(-2:2, rnorm, n = 5)
+
+#5
+mtcars %>% map(x, function(df) lm(mpg ~ wt, data = df))
+mtcars %>% map(~lm(mpg ~ wt, data=.))
+
+# Dealing with Failure
+
+safe_log <- safely(log)
+str(safe_log(10))
+str(safe_log("a"))
+
+x <- list(1, 10, "a")
+y <- x %>% map(safely(log))
+str(y)
+
+y <- y %>% transpose()
+str(y)
+
+is_ok <- y$error %>% map_lgl(is_null)
+x[!is_ok]
+y$result[is_ok] %>% flatten_dbl()
+
+x <- list(1, 10, "a")
+x %>% map_dbl(possibly(log, NA_real_))
+
+x <- list(1, -1)
+x %>% map(quietly(log)) %>% str()
+
+# Mapping over Multiple Arguments
+
+mu <- list(5, 10, -3)
+mu %>% 
+  map(rnorm, n = 5) %>% 
+  str()
+
+sigma <- list(1, 5, 10)
+seq_along(mu) %>% 
+  map(~rnorm(5, mu[[.]], sigma[[.]])) %>% 
+  str()
+
+map2(mu, sigma, rnorm, n = 5) %>% str()
+
+map2 <- function(x, y, f, ...) {
+  out <- vector("list", length(x))
+  for (i in seq_along(x)) {
+    out[[i]] <- f(x[[i]], y[[i]], ...)
+  }
+  out
+}
+
+n <- list(1, 3, 5)
+args <- list(n, mu, sigma)
+args %>% 
+  pmap(rnorm) %>% 
+  str()
+
+args2 <- list(mean = mu, sd = sigma, n = n)
+args2 %>% 
+  pmap(rnorm) %>% 
+  str()
+
+params <- tribble(
+  ~mean, ~sd, ~n,
+  5,      1,   1,
+  10,     5,   3,
+  -3,    10,   5)
+
+params %>% 
+  pmap(rnorm) %>% 
+  str()
+
+## Invoking Different Functions
+
+f <- c("runif", "rnorm", "rpois")
+param <- list(
+  list(min = -1, max = 1),
+  list(sd = 5),
+  list(lambda = 10)
+)
+invoke_map(f, param, n = 5) %>% str()
+
+sim <- tribble(
+  ~f,      ~params,
+  "runif", list(min = -1, max = 1),
+  "rnorm", list(sd = 5),
+  "rpois", list(lambda = 10)
+)
+sim_tibble <- sim %>% 
+  mutate(sim = invoke_map(f, params, n = 10))
+
+sim_tibble$params
+
+# Walk
+
+x <- list(1, "a", 3)
+x %>% 
+  walk(print)
+
+plots <- mtcars %>% 
+  split(.$cyl) %>% 
+  map(~ggplot(., aes(mpg, wt)) + geom_point())
+paths <- stringr::str_c(names(plots), ".pdf")
+pwalk(list(paths, plots), ggsave, path = tempdir())
+
+pdfFonts()
+
+# Other Patterns of For Loops
+
+## Predicate Functions
+
+iris %>% 
+  keep(is.factor) %>% 
+  str()
+
+iris %>% 
+  discard(is.factor) %>% 
+  str()
+
+x <- list(1:5, letters, list(10))
+x
+x %>% 
+  some(is_character)
+
+x %>% 
+  every(is_vector)
+
+x <- sample(10)
+
+x %>% 
+  detect(~ . > 5)
+
+x %>% 
+  detect_index(~ . > 5)
+
+?detect_index
+
+x %>% 
+  head_while(~ . > 5)
+x %>% 
+  tail_while(~ . > 5)
+
+## Reduce and Accumulate
+
+dfs <- list(
+  age = tibble(name = "John", age = 30),
+  sex = tibble(name = c("John", "Mary"), sex = c("M", "F")),
+  trt = tibble(name = "Mary", treatment = "A")
+)
+
+dfs %>% reduce(full_join)
+
+vs <- list(
+  c(1, 3, 5, 6, 10),
+  c(1, 2, 3, 7, 8, 10),
+  c(1, 2, 3, 4, 8, 9, 10)
+)
+vs %>% reduce(intersect)
+
+(x <- sample(10))
+x
+x %>% accumulate(`+`)
+
+## Exercises
+
+#1 
+mtcars %>% every(is_vector)
+
+output <- vector("double", ncol(mtcars))
+output
+for (i in seq_along(mtcars)) {
+  if(!is_vector(mtcars[[i]])) print(F) 
+  print(T)
+}
+
+every2 <- function(.x, .p, ...) {
+  for (i in .x) {
+    if (!.p(i, ...)) {
+      # If any is FALSE we know not all of then were TRUE
+      return(FALSE)
+    }
+  }
+  # if nothing was FALSE, then it is TRUE
+  TRUE  
+}
+
+#2
+
+col_sum2 <- function(df, f, ...) {
+  map(keep(df, is.numeric), f, ...)
+}
+
+col_sum2(df, mean)
+
+#3
+col_sum3 <- function(df, f) {
+  is_num <- sapply(df, is.numeric)
+  df_num <- df[, is_num]
+  
+  sapply(df_num, f)
+}
+
+df <- tibble(
+  x = 1:3,
+  y = 3:1,
+  z = c("a", "b", "c")
+)
+
+col_sum3(df, mean)
+col_sum3(df[1:2], mean)
+col_sum3(df[1], mean)
+col_sum3(df[0], mean)
+
