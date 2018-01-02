0e002bea93e5daa431ab2c584ad3e555352e17b2
Harry Yang <harry.seunghoon.yang@gmail.com>
2018-01-02 15:52:27 +0900

    chapter 18 cleared

--- codes/chapter18.R
+++ codes/chapter18.R
@@ -275,3 +275,217 @@ ggplot(sim1, aes(x, resid)) +
 ?gather_predictions
 
 # Formulas and Model Families
+
+df <- tribble(
+  ~y, ~x1, ~x2,
+  4,   2,   5,
+  5,   1,   6)
+
+df
+model_matrix(df, y~x1)
+
+model_matrix(df, y ~ x1 - 1)
+
+model_matrix(df, y ~ x1 + x2)
+
+## Categorical Variables
+
+df <- tribble(
+  ~ sex, ~ response,
+    "male", 1,
+    "female", 2,
+    "male", 1
+)
+df
+model_matrix(df, response ~ sex)
+
+ggplot(sim2) +
+  geom_point(aes(x, y))
+
+mod2 <- lm(y ~ x, data = sim2)
+grid <- sim2 %>% 
+  data_grid(x) %>% 
+  add_predictions(mod2)
+
+ggplot(sim2, aes(x)) +
+  geom_point(aes(y = y)) +
+  geom_point(
+    data = grid,
+    aes(y = pred),
+    color = "red",
+    size = 4
+  )
+
+tibble(x = 'e') %>% 
+  add_predictions(mod2)
+
+## Interactions (Continuous and Categorical)
+
+ggplot(sim3, aes(x1, y)) +
+  geom_point(aes(color = x2))
+
+sim3
+
+mod1 <- lm(y ~ x1 + x2, data = sim3)
+mod2 <- lm(y ~ x1 * x2, data = sim3)
+
+grid <- sim3 %>% 
+  data_grid(x1, x2) %>% 
+  gather_predictions(mod1, mod2)
+grid
+
+ggplot(sim3, aes(x1, y, color = x2)) +
+  geom_point() +
+  geom_line(data = grid, aes(y = pred)) +
+  facet_wrap(~model)
+
+sim3 <- sim3 %>% 
+  gather_residuals(mod1, mod2)
+
+ggplot(sim3, aes(x1, resid, color = x2)) +
+  geom_point() +
+  facet_grid(model ~ x2)
+
+## Interactions (Two Continuous)
+
+mod1 <- lm(y ~ x1 + x2, data = sim4)
+mod2 <- lm(y ~ x1 * x2, data = sim4)
+
+grid <- sim4 %>% 
+  data_grid(
+    x1 = seq_range(x1, 5),
+    x2 = seq_range(x2, 5)
+  ) %>% 
+  gather_predictions(mod1, mod2)
+
+seq_range(c(0.0123, 0.923423), n = 5)
+seq_range(c(0.0123, 0.923423), n = 5, pretty = T)
+
+x1 <- rcauchy(100)
+seq_range(x1, n = 5)
+seq_range(x1, n = 5, trim = 0.10)
+seq_range(x1, n = 5, trim = 0.25)
+seq_range(x1, n = 5, trim = 0.50)
+
+x2 <- c(0, 1)
+seq_range(x2, n = 5)
+seq_range(x2, n = 5, expand = 0.10)
+seq_range(x2, n = 5, expand = 0.25)
+seq_range(x2, n = 5, expand = 0.50)
+
+ggplot(grid, aes(x1, x2)) +
+  geom_tile(aes(fill = pred)) +
+  facet_wrap(~ model)
+
+ggplot(grid, aes(x1, pred, color = x2, group = x2)) +
+  geom_line() +
+  facet_wrap(~ model)
+
+ggplot(grid, aes(x2, pred, color = x1, group = x1)) +
+  geom_line() +
+  facet_wrap(~ model)
+
+## Transformations
+
+df <- tribble(
+  ~y, ~x,
+   1,  1,
+   2,  2,
+   3,  3)
+
+model_matrix(df, y ~ x^2 + x)
+model_matrix(df, y ~ I(x^2) + x)
+model_matrix(df, y ~ poly(x, 2))
+
+library(splines)
+model_matrix(df, y ~ ns(x, 2))
+
+sim5 <- tibble(
+  x = seq(0, 3.5 * pi, length = 50),
+  y = 4 * sin(x) + rnorm(length(x))
+)
+
+ggplot(sim5, aes(x, y)) +
+  geom_point()
+
+mod1 <- lm(y ~ ns(x, 1), data = sim5)
+mod2 <- lm(y ~ ns(x, 2), data = sim5)
+mod3 <- lm(y ~ ns(x, 3), data = sim5)
+mod4 <- lm(y ~ ns(x, 4), data = sim5)
+mod5 <- lm(y ~ ns(x, 5), data = sim5)
+
+grid <- sim5 %>% 
+  data_grid(x = seq_range(x, n = 50, expand = 0.1)) %>% 
+  gather_predictions(mod1, mod2, mod3, mod4, mod5, .pred = "y")
+
+ggplot(sim5, aes(x, y)) +
+  geom_point() +
+  geom_line(data = grid, color = "red") +
+  facet_wrap(~ model)
+
+## Exercises
+
+#1
+mod1 <- lm(y~x - 1, data = sim2)
+mod2 <- lm(y~x, data = sim2)
+mod1$coefficients
+mod2$coefficients
+
+grid1 <- sim2 %>% 
+  data_grid(x) %>% 
+  add_predictions(mod1)
+grid2 <- sim2 %>% 
+  data_grid(x) %>% 
+  add_predictions(mod2)
+grid1
+grid2
+grid3 <- sim2 %>% 
+  data_grid(x) %>% 
+  gather_predictions(mod1, mod2)
+
+ggplot(sim2, aes(x)) +
+  geom_point(aes(y=y)) +
+  geom_point(data=grid3, aes(y = pred), color = "red", size = 4) +
+  facet_grid(~model)
+
+#2
+model_matrix(sim3, y ~ x1 + x2)
+model_matrix(sim3, y ~ x1 * x2)
+model_matrix(sim4, y ~ x1 + x2)
+model_matrix(sim4, y ~ x1 * x2)
+
+model_matrix(sim3, y ~ x2 * I(x1 ^ 2))
+
+#3
+#4
+ggplot(sim4, aes(x1, y)) +
+  geom_point() +
+  geom_line(data = grid, color = "red") +
+  facet_wrap(~ model)
+
+sim4
+grid <- sim4 %>% 
+  data_grid(x1, x2) %>% 
+  gather_residuals(mod1, mod2)
+
+ggplot(sim4, aes(x1, x2)) +
+  geom_smooth(data = grid, aes(color = model)) +
+  facet_grid(~ model)
+
+table(grid$model)
+
+# Missing Values
+
+df <- tribble(
+  ~x, ~y,
+  1, 2.2,
+  2, NA,
+  3, 3.5,
+  4, 8.3, 
+  NA, 10
+)
+mod <- lm(y ~ x, data = df)
+mod <- lm(y ~ x, data = df, na.action = na.exclude)
+nobs(mod)
+
+# Other Model Families
